b2438e3a1b5b1f7f061cf2763d6caa85a026fd11
Peter Platis <xxplatispeterxx@gmail.com>
2024-05-01 22:24:50 +0300

    done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputString = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
+const root = document.querySelector(":root");
 
 checkButton.addEventListener("click", () => {
 	checkPalindrome(inputString.value);
@@ -11,12 +12,22 @@ function checkPalindrome(inStr) {
 		alert("Please input a value");
 	} else {
 		let cleanString = inStr.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-		let invertedString = cleanString.split("").reverse().join('');
+		let invertedString = cleanString.split("").reverse().join("");
 
 		if (cleanString === invertedString) {
 			result.innerText = `${inStr} is a Palindrome`;
+			makeSplashGreen();
 		} else {
 			result.innerText = `${inStr} is not a Palindrome`;
+			makeSplashRed();
 		}
 	}
+}
+
+function makeSplashGreen() {
+	root.style.setProperty("--splash", "var(--green)");
+}
+
+function makeSplashRed() {
+	root.style.setProperty("--splash", "var(--red)");
 }
